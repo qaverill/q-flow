1208481b0f30ac5e183cae3cf0a57150af8ec303
Quinn Averill <qaverill15@gmail.com>
2020-12-05 19:41:01 -0500

    slowly adding in structural things

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import { NotificationContainer } from 'react-notifications';
+import App from './app';
+import { StoreContext, reducer, initialState } from './store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const Root = () => {
+  const [state, dispatch] = React.useReducer(reducer, { ...initialState });
+  return (
+    <Router>
+      <NotificationContainer />
+      <StoreContext.Provider value={{ state, dispatch }}>
+        <App />
+      </StoreContext.Provider>
+    </Router>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ frontend/src/utils/index.js
@@ -0,0 +1,37 @@
+// ----------------------------------
+// HELPERS
+// ----------------------------------
+export const ONE_EPOCH_DAY = 86400;
+const getMonth = date => (date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1);
+const getDate = date => (date.getDate() < 10 ? `0${date.getDate()}` : date.getDate());
+// ----------------------------------
+// CONVERTERS
+// ----------------------------------
+export const epochToDate = (epoch) => new Date(epoch * 1000);
+export const dateToEpoch = (date) => parseInt(new Date(date).getTime() / 1000, 10);
+export const dateToString = (date) => `${getMonth(date)}/${getDate(date)}/${date.getFullYear()}`;
+export const stringToEpoch = (s) => Math.round(new Date(`${s.split('/')[2]}-${s.split('/')[0]}-${s.split('/')[1]}T00:00:00Z`).getTime() / 1000);
+export const epochToString = (epoch) => epoch != null && dateToString(new Date(epoch * 1000));
+export const msToString = (durationInMs) => {
+  const hours = parseInt(durationInMs / 3600000, 10);
+  const minutes = parseInt(((durationInMs / 1000) % 3600) / 60, 10);
+  const seconds = parseInt((durationInMs / 1000) % 60, 10);
+  return `${hours < 10 ? `0${hours}` : hours}h ${minutes < 10 ? `0${minutes}` : minutes}m ${seconds < 10 ? `0${seconds}` : seconds}s`;
+};
+
+export const times = {
+  firstOfCurrentMonth: () => Math.round(
+    new Date(
+      new Date().getFullYear(),
+      new Date().getMonth() - 7,
+      1,
+    ) / 1000,
+  ),
+  now: () => Math.round(new Date().getTime() / 1000),
+  getNameOfMonth: timestamp => {
+    const date = epochToDate(timestamp);
+    const month = date.toLocaleString('default', { month: 'long' });
+    return month;
+  },
+  getMonthAndYear: date => `${getMonth(date)}/${date.getFullYear().toString().slice(2)}`,
+};
